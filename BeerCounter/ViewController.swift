c81c1291c9d5d2134bbfec2bfe5d2cb69e0e1cd0
Fernando <lf.fernandosantos@gmail.com>
2019-05-23 14:56:33 -0300

    Initial Commit

---
+++ BeerCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerCounter
+//
+//  Created by Fernando on 23/05/19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
